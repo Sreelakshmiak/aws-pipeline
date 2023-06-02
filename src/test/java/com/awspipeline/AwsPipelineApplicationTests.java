daec76c1e75093057e94842e54eeec5d8271ae89
Sreelakshmiak <245277@ust.com>
2023-06-02 16:43:13 +0530

    first commit

---
+++ src/test/java/com/awspipeline/AwsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
